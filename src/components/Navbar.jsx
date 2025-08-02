46a6dfc87b94300755125634dc59288c7e5cb173
marilynsaravia <goitiasaraviamarilyn@gmail.com>
2025-08-02 17:54:10 +0200

    Update footer text to English and add sample notes.json file

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { getImageNavbar } from "../utils"
 const Navbar = () => {
     return(
         <div className='w-full h-[130px] flex justify-start items-center px-12'>
-            <img className='w-[150px]' src={getImageNavbar('logo.png')} alt='Logotipo Notepad App'/>
+            <img className='w-[150px]' src={getImageNavbar('logo.png')} alt='Notepad App Logo'/>
         </div>
     )
 };
